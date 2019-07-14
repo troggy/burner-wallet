fd874ac27dd79371f6ce8ea1dae5db64db4087f4
Maksim Daunarovich <max.daunarovich@gmail.com>
2019-07-14 12:14:55 +0300

    Create helpers to operate localStorage

---
+++ src/services/localStorage.js
@@ -0,0 +1,17 @@
+export const localStorageExists =
+  localStorage && typeof localStorage.setItem === "function";
+
+const getFieldName = (name, account) => (account ? `${account}${name}` : name);
+
+export const getStoredValue = (name, account) =>
+  localStorage.getItem(getFieldName(name, account));
+
+export const storeValues = (params, account) => {
+  const keys = Object.keys(params);
+  const values = Object.values(params);
+  keys.forEach((key, index) => {
+    const value = values[index];
+    const name = getFieldName(key, account);
+    localStorage.setItem(name, value);
+  });
+};
